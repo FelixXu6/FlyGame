bc7372b7d71c361e732967f7d76e074385060ed8
Zhang's husband <1579469572@qq.com>
2023-06-18 00:05:56 +0800

    project init

---
+++ FlyGame/src/main/java/com/xzhangu/flygame/FlyGameApplication.java
@@ -0,0 +1,13 @@
+package com.xzhangu.flygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyGameApplication.class, args);
+	}
+
+}
---
+++ FlyGame/src/test/java/com/xzhangu/flygame/FlyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzhangu.flygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
